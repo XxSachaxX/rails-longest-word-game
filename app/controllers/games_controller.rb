8d8afb0e625dccd9661abd9e1a327bc2f1307159
Sacha Gotainer <sgotainer@hotmail.com>
2022-08-15 15:59:15 +0200

    Done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,27 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = ('a'..'z').to_a.sample(10)
+  end
+
+  def score
+    @attempt = params[:word]
+    @letters = params[:letters]
+    url = "https://wagon-dictionary.herokuapp.com/#{@attempt}"
+    word_serialized = URI.open(url).read
+    @word = JSON.parse(word_serialized)
+    if !include?(@attempt, @letters)
+      @score = "Sorry but #{@attempt} can't be built from #{@letters} "
+    elsif !@word['found']
+      @score = "Sorry but #{@attempt} does not seem to be a valid ENGLISH word"
+    else
+    @score = "Congratulations, #{@attempt} is a Valid english word"
+    end
+  end
+
+  def include?(attempt, letters)
+    attempt.chars.all? { |letter| attempt.count(letter) <= letters.count(letter) }
+  end
+end
